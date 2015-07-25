1a3828aa44a9f1905f19f1e1c20b32b98c25a234
Brajesh Kumar <kbrajesh176@gmail.com>
2015-07-25 19:37:58 +0530

    adding new dashboard background

--- app/src/main/java/com/pragatiplusplus/tictactoe/DashBoard.java
+++ app/src/main/java/com/pragatiplusplus/tictactoe/DashBoard.java
@@ -1,5 +1,6 @@
 package com.pragatiplusplus.tictactoe;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.EditText;
 
 
-public class DashBoard extends ActionBarActivity {
+public class DashBoard extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,6 @@ public class DashBoard extends ActionBarActivity {
         setContentView(R.layout.activity_dash_board);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dash_board, menu);
-        return true;
-    }
 
     public void startPlay(View v)
     {
@@ -41,18 +36,5 @@ public class DashBoard extends ActionBarActivity {
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
