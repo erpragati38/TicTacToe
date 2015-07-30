6a70a592f0d8581e8f8e983f1fb5bdc66ce75b04
Pragati Agarwal <erpragati38@gmail.com>
2015-07-31 00:49:09 +0530

    Created Interface  and  Abstract Class. 8 Commit

---
+++ app/src/main/java/com/pragatiplusplus/tictactoe/ai/ComputerAI.java
@@ -0,0 +1,12 @@
+package com.pragatiplusplus.tictactoe.ai;
+
+import com.pragatiplusplus.tictactoe.ui.view.TTTView;
+
+/**
+ * Created by Pragati on 7/31/2015.
+ */
+public interface ComputerAI {
+
+    public  int[] getMove(TTTView TTTButton[][]);
+
+}
---
+++ app/src/main/java/com/pragatiplusplus/tictactoe/ai/absLevel.java
@@ -0,0 +1,14 @@
+package com.pragatiplusplus.tictactoe.ai;
+
+import com.pragatiplusplus.tictactoe.ui.view.TTTView;
+
+/**
+ * Created by Pragati on 7/31/2015.
+ */
+public abstract class absLevel implements ComputerAI {
+
+    @Override
+    public int[] getMove(TTTView[][] TTTButton) {
+return null;
+    }
+}
